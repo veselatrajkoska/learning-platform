2a553fe84f573f03804c2f6b35ba849f0260fe9a
veselatrajkoska <89221046+veselatrajkoska@users.noreply.github.com>
2022-08-30 15:25:34 +0200

    Update settings.py

--- hci_final_hw/hci_final_hw/settings.py
+++ hci_final_hw/hci_final_hw/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-qk500g0db2&!4-w9p+s)wlw64_8-ekq$(p-mrk0-n*+3(n3k)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['veselatrajkoska.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'veselatrajkoska.pythonanywhere.com',
+    '127.0.0.1',
+]
 
 # Application definition
 
